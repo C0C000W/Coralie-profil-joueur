b18fd377d0eba0f419c1ec0ca5c1f76545def9f7
C0C000W <coralie.fonlupt.simplon@gmail.com>
2022-02-28 15:29:57 +0100

    ajout clé API qui ne marche pas

--- getProfil.php
+++ getProfil.php
@@ -1,6 +1,6 @@
 <?php
 
-	$response = @file_get_contents("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/".$_GET['name']."?api_key=RGAPI-ce47c040-0b72-42a2-bc42-e4193d453478");
+	$response = @file_get_contents("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/".$_GET['name']."?api_key=RGAPI-652e41fa-935f-4893-8e67-09b715dfc91c");
 
 if ($response === FALSE){
 	$response = '{"id": "none","accountId": "none","puuid": "none","name": "inconnu","profileIconId": 0,"revisionDate": 0, "summonerLevel": 0}';
